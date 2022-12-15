6da14bf6e0fa3fe566f6348be9d346c5849dd7eb
Nicola <lanzoni.nicola@gmail.com>
2022-12-15 18:15:06 -0300

    fix: dialogwrapper position

--- website/app/client/templates-editor/countdown-widget/index.tsx
+++ website/app/client/templates-editor/countdown-widget/index.tsx
@@ -6,18 +6,17 @@ import CountdownWidgetStyleTag from "./components/countdown-widget-style-tag/cou
 
 export default function CountdownWidget() {
   return (
-    <Iframe
-      style={{ width: "100%", height: "100%", border: "0px" }}
-      data-element="countdown-widget"
-    >
-      {/* <style>{`body {margin: 0;padding: 0;overflow: hidden;box-sizing: border-box;position: relative;`}</style> */}
-      <CountdownWidgetLink>
+    <CountdownWidgetLink>
+      <Iframe
+        style={{ width: "100%", height: "100%", border: "0px" }}
+        data-element="countdown-widget"
+      >
         <CountdownWidgetGoogleFontTag />
         <div data-element="countdown-container">
           <Countdown />
         </div>
         <CountdownWidgetStyleTag />
-      </CountdownWidgetLink>
-    </Iframe>
+      </Iframe>
+    </CountdownWidgetLink>
   );
 }
